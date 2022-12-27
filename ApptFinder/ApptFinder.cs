60c3077a8639bd7e8f4591cccb33e18ad2d44e41
marty <martin.d.weel@icloud.com>
2022-12-27 11:25:59 -0800

    init

--- ApptFinder/ApptFinder.cs
+++ ApptFinder/ApptFinder.cs
@@ -2,8 +2,81 @@
 
 namespace ApptFinder;
 
+public class Resource
+{
+    public string Name { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public List<Appointment> Appointments { get; set; }
+
+    public Resource(string name, DateTime startTime, DateTime endTime)
+    {
+        Name = name;
+        StartTime = startTime;
+        EndTime = endTime;
+        Appointments = new List<Appointment>();
+    }
+}
+
+public class Appointment
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public Appointment(DateTime startTime, DateTime endTime)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+}
+
 public class ApptFinder
 {
 
+   
+        public List<DateTime> GetAvailableTimeSlots(Resource resource, TimeSpan duration, TimeSpan appointmentDuration)
+        {
+            // Create a list to store the available time slots
+            var availableTimeSlots = new List<DateTime>();
+
+            // Start with the resource's start time
+            var currentTime = resource.StartTime;
+
+            // Loop through the day, checking for available time slots
+            while (currentTime < resource.EndTime)
+            {
+                // Check if the current time is available
+                if (IsTimeSlotAvailable(resource, currentTime, appointmentDuration))
+                {
+                    // If it is, add it to the list of available time slots
+                    availableTimeSlots.Add(currentTime);
+                }
+
+                // Increment the current time by the desired duration
+                currentTime = currentTime.Add(duration);
+            }
+
+            // Return the list of available time slots
+            return availableTimeSlots;
+        }
+
+        private bool IsTimeSlotAvailable(Resource resource, DateTime startTime, TimeSpan duration)
+        {
+            // Check each appointment to see if it conflicts with the desired time slot
+            foreach (var appointment in resource.Appointments)
+            {
+                if (startTime + duration > appointment.StartTime && startTime < appointment.EndTime)
+                {
+                    // If there is a conflict, the time slot is not available
+                    return false;
+                }
+            }
+
+            // If there are no conflicts, the time slot is available
+            return true;
+        }
+    
 }
+
+
 
---
+++ ApptFinder_Test/ApptFinderTest.cs
@@ -0,0 +1,40 @@
+﻿using ApptFinder;
+using System.Collections.Generic;
+
+namespace ApptFinder_Test;
+
+public class ApptFinderTest
+{
+    [Fact]
+    public void FindAvailAppt()
+    {
+
+        var resource = new Resource("Conference Room 1", new DateTime(2022, 1, 1, 8, 0, 0), new DateTime(2022, 1, 1, 17, 0, 0));
+
+        // Add some appointments to the resource's Appointments list
+        resource.Appointments.Add(new Appointment(new DateTime(2022, 1, 1, 10, 0, 0), new DateTime(2022, 1, 1, 11, 0, 0)));
+        resource.Appointments.Add(new Appointment(new DateTime(2022, 1, 1, 13, 0, 0), new DateTime(2022, 1, 1, 14, 00, 0)));
+        resource.Appointments.Add(new Appointment(new DateTime(2022, 1, 1, 15, 0, 0), new DateTime(2022, 1, 1, 16, 0, 0)));
+
+        // Create a Scheduler object
+        var apptFinder = new ApptFinder.ApptFinder();
+
+        // Get the available time slots at 15-minute intervals, each with a duration of 1 hour
+        TimeSpan intervalDuration = new TimeSpan(0, 15, 0); // 15 minutes
+        TimeSpan appointmentDuration = new TimeSpan(1); // 1 hour
+        List<DateTime> availableTimeSlots = apptFinder.GetAvailableTimeSlots(resource, intervalDuration, appointmentDuration);
+
+        // Print the available time slots
+        Console.WriteLine("Available time slots:");
+
+        var i = 0;
+        foreach (var timeSlot in availableTimeSlots)
+        {
+            Console.WriteLine(timeSlot.ToString());
+            i++;
+        }
+
+        Assert.Equal(i, 24);
+
+    }
+}
